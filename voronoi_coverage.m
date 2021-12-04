82a3a5587c4256fa89cf2001a021d5f3579eb030
aswingururaj <aswingururaj1998@yahoo.com>
2021-12-04 16:00:46 -0500

    updated voronoi coverage

--- voronoi_coverage.m
+++ voronoi_coverage.m
@@ -1,3 +1,36 @@
 function [x,y,data] = voronoi_coverage(x,y,data)
-
+[rows,cols] = size(data);
+[no_of_robots,]= size(x);
+centroids = [x;y];
+classifications = zeros(rows,cols);
+max_iter = 100;
+loss = 0;
+for i = 1:max_iter
+    for j=1:rows
+        for k=1:cols
+            distance_from_centroid = zeros(no_of_robots);
+            for l=1:no_of_robots
+                distance_from_centroid(l) = sqrt(((j-centroids(l,1))^2)+((k-centroids(l,2))^2));
+            end
+            [M,classifications(j,k)] = min(distance_from_centroid);
+        end
+    end
+    new_centroid = zeros(no_of_robots,2)
+    new_loss = 0
+    for m=1:no_of_robots
+        same_voronoi_cell = find(classifications == m);
+        new_centroid(m,:)=mean(same_voronoi_cell);
+        data(round(new_centroid(m,1)),round(new_centroid(m,2))) = 255;
+        for n=1:rows
+            for o=1:cols
+                new_loss = new_loss + sqrt(((n-centroids(m,1))^2)+((o-centroids(m,2))^2));
+            end
+        end
+    end
+    if abs(new_loss-loss) < 0.001
+        break
+    end
+    centroids = new_centroid;
+    loss = new_loss;
+end
 end
